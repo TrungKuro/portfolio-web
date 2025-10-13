8aab6fd1dbce43c20f45bb1a7d8d5817a812c500
Trung Hoang <hdh.trung96@gmail.com>
2025-10-13 16:36:03 +0700

    Remove 'ScrollRevealAnimation' effect for 'About' section

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { ScrollRevealAnimation } from "../layout/ScrollRevealAnimation";
 import { HeadingHighlight } from "../common/HeadingHighlight";
 import { BentoGrid, BentoGridItem } from "../ui/BentoGrid";
 import { cn } from "@/lib/utils";
@@ -10,64 +9,62 @@ export const About = ({ id }: { id: string }) => {
 
   return (
     <section id={id}>
-      <ScrollRevealAnimation>
-        <div className="flex flex-col items-center pt-30">
-          <HeadingHighlight
-            title={about.title}
-            wordHighlight={about.titleHighlight}
-          />
+      <div className="flex flex-col items-center pt-30">
+        <HeadingHighlight
+          title={about.title}
+          wordHighlight={about.titleHighlight}
+        />
 
-          <BentoGrid className={cn("mx-auto mt-10 w-full", bentoGridClassName)}>
-            {bentoGridItems.map(
-              (
-                {
-                  className,
-                  //
-                  title,
-                  description,
-                  contentClassName,
-                  titleClassName,
-                  descriptionClassName,
-                  //
-                  img,
-                  spareImg,
-                  imgH,
-                  imgW,
-                  spareImgH,
-                  spareImgW,
-                  imgClassName,
-                  spareImgClassName,
-                  //
-                  specialEffect,
-                },
-                idx,
-              ) => (
-                <BentoGridItem
-                  key={`${idx}-${title}`}
-                  className={className}
-                  //
-                  title={title}
-                  description={description}
-                  contentClassName={contentClassName}
-                  titleClassName={titleClassName}
-                  descriptionClassName={descriptionClassName}
-                  //
-                  img={img}
-                  spareImg={spareImg}
-                  imgH={imgH}
-                  imgW={imgW}
-                  spareImgH={spareImgH}
-                  spareImgW={spareImgW}
-                  imgClassName={imgClassName}
-                  spareImgClassName={spareImgClassName}
-                  //
-                  specialEffect={specialEffect}
-                />
-              ),
-            )}
-          </BentoGrid>
-        </div>
-      </ScrollRevealAnimation>
+        <BentoGrid className={cn("mx-auto mt-10 w-full", bentoGridClassName)}>
+          {bentoGridItems.map(
+            (
+              {
+                className,
+                //
+                title,
+                description,
+                contentClassName,
+                titleClassName,
+                descriptionClassName,
+                //
+                img,
+                spareImg,
+                imgH,
+                imgW,
+                spareImgH,
+                spareImgW,
+                imgClassName,
+                spareImgClassName,
+                //
+                specialEffect,
+              },
+              idx,
+            ) => (
+              <BentoGridItem
+                key={`${idx}-${title}`}
+                className={className}
+                //
+                title={title}
+                description={description}
+                contentClassName={contentClassName}
+                titleClassName={titleClassName}
+                descriptionClassName={descriptionClassName}
+                //
+                img={img}
+                spareImg={spareImg}
+                imgH={imgH}
+                imgW={imgW}
+                spareImgH={spareImgH}
+                spareImgW={spareImgW}
+                imgClassName={imgClassName}
+                spareImgClassName={spareImgClassName}
+                //
+                specialEffect={specialEffect}
+              />
+            ),
+          )}
+        </BentoGrid>
+      </div>
     </section>
   );
 };
